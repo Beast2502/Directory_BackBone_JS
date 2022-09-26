13241d3ae9721c000afb0a048b413f38a78155cc
Mehul Saxena <mehulsaxena45@gmail.com>
2022-09-26 19:12:58 +0530

    Check

--- js/models/person-model.js
+++ js/models/person-model.js
@@ -1,29 +1,32 @@
 app = app || {};
 
 app.models.Person = Backbone.Model.extend({
-    defaults: {
-        'ID' : '',
-        'firstname' : '',
-        'lastname' : '',
-        'homephone' : '',
-        'email' : '',
-        'parent' : ''
-    },
-    initialize : function(){
-        let self = this;
-        if(this.get('person') !== ''){
-            self.set('type' ,'student');
-        } else {
-            self.set('type' , 'parent');
-        }
-
-    }
-})
-
+	defaults: {
+		'ID': '',
+		'firstname': '',
+		'lastname': '',
+		'homephone': '',
+		'email': '',
+		'parent': ''
+	},
+	
+	initialize: function() {
+		var self = this;
+		if(this.get('parent') !== '') {
+			self.set('type', 'student');
+		} else {
+			self.set('type', 'parent');
+		}
+	}
+	
+});
 
 app.collections.People = Backbone.Collection.extend({
-    model : app.models.Person,
-    comparator : function(person){
-        return person.get('latname');
-    }
-})
+	
+	model: app.models.Person,
+	
+	comparator: function(person) {
+		return person.get('lastname');
+	}
+	
+});
