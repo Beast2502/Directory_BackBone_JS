c7adf1e53a8a3e3a16ad42df59eb68865db473ef
Mehul Saxena <MSaxena@Arenaco.com>
2022-09-26 18:21:40 +0530

    check

--- js/views/person-views.js
+++ js/views/person-views.js
@@ -7,11 +7,19 @@ app.views.Person = Backbone.View.extend({
             class : 'person' + this.model.get('type')
         }
     },
+    events: {
+        'click .list-header' : 'showDetails'
+    } ,
 
     template : _.template($('#person-template').html()),
     render: function(){
         this.$el.html(this.template(this.model.toJSON()));
         return this;
+    },
+    showDetails : function(e){
+        $(e.target).toggleClass('active');
+        $(e.target).siblings('.details').slideToggle('fast');
+
     }
 });
 
@@ -20,6 +28,11 @@ app.views.People = Backbone.View.extend({
     initialize : function(data){
         this.collection = new app.collections.People(data);
         this.render();
+        this.on('change:searchFilter' , this.filterBySearch,this);
+        this.collection.on("reset" , this.render , this);
+    },
+    events:{
+        'keyup #searhBox' : 'serachFilter'
     },
     render : function(){
         let self = this;
@@ -33,5 +46,17 @@ app.views.People = Backbone.View.extend({
             model:person
         });
         $('#listing').append(newPerson.render().el);
+    },
+    serachFilter : function(e){
+        this.serachFilter = e.target.value;
+        this.trigger('change:searchFilter');
+    },
+    filterBySearch: function(){
+        this.collection.reset(directoryData ,{silent:true});
+        let filterString = this.serachFilter, 
+        filtered = _.filter(this.collection.models, function(item){
+            return item.get('lastname').toLowerCase().indexOf(filterString.toLowerCase()) !== -1;
+        })
+        this.collection.reset(filtered);
     }
 })
